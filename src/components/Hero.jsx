fd5e30e07be635d3d87e77f3b2e0afe52091cf7e
rshdevmachine <152737810+rshdevmachine@users.noreply.github.com>
2024-10-22 00:04:42 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import Button from '@mui/material/Button';
 import profilePic from "../assets/myimage.jpg";
 import { FaDownload } from "react-icons/fa";
-import resume from "../assets/FinalResume.pdf";
+import resume from "../assets/Resume_arsh.pdf";
 import { Spotlight } from "./Spotlight.jsx";
 import { FlipWords } from "./Flip.jsx";
 import { ButtonsCard } from "./Buttons.jsx";
